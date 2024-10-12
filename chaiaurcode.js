369437cab512754aa5aee7b014b2294cfbf7e9b3
anat khan <147741390+salmasiddique12@users.noreply.github.com>
2024-10-12 16:24:29 +0530

    Add files via upload

---
+++ chaiaurcode.js
@@ -0,0 +1,15 @@
+// const clock=document.querySelector('#clock')
+const clock=document.getElementById('clock');
+
+// console.log(clock);
+
+// console.log(date.toLocaleString());
+
+setInterval(function () {
+
+    let date=new Date();
+    
+    //  console.log(date.toLocaleString());
+    clock.innerHTML=date.toLocaleTimeString();
+    
+},1000);
